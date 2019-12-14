1ce18e8138644f67af6cdd19178b307a5a299ccb
Ho Yin Keung <hykeung92@gmail.com>
2019-12-14 00:26:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,9 @@ end
 
 def long_planeteer_calls(array) # code an argument here
   # Your code here
-  array.any? do |i|
-    i.length > 4
+  # array.any? do |i|
+  #   i.length > 4
+  
   end
 end
 
