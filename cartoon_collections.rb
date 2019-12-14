c69463e94d83ebbb151d95867c10c2e152288d3d
Ho Yin Keung <hykeung92@gmail.com>
2019-12-14 00:27:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,8 @@ def long_planeteer_calls(array) # code an argument here
   # Your code here
   # array.any? do |i|
   #   i.length > 4
-  array.any?{|call| call.length > 4}
-  end
+  a  endrray.any?{|call| call.length > 4}
+
 end
 
 def find_the_cheese(cheese)# code an argument here
